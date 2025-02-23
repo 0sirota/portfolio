19e6eeb0200679202ffde01063f945d4b5ecc878
0sirota <oliver.sirota@gmail.com>
2025-02-23 21:59:07 +1100

    Disabled resume section for security reasons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function App() {
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
-      <ResumeSection />
+      {/* <ResumeSection /> */}
       {/* <MacbookScrollDemo /> */}
       <ContactSection />
 
